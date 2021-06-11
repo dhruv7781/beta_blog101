2fb5d7102905ce2b184fb99bf8fb65dbfc2b49a2
Dhruv Kapoor <dhruvkapoor01@gmail.com>
2021-06-11 17:29:03 +0530

    form partial commit

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,6 +1,7 @@
 class ArticlesController < ApplicationController
+before_action :set_article, only: [:show, :edit, :update, :destroy]
   def show
-    @article = Article.find(params[:id])
+    
 
   end
 
@@ -13,7 +14,7 @@ class ArticlesController < ApplicationController
   end
 
   def edit
-    @article = Article.find(params[:id])
+    
   end
 
   def create
@@ -27,7 +28,7 @@ class ArticlesController < ApplicationController
   end
 
   def update
-    @article = Article.find(params[:id])
+   
     if @article.update(params.require(:article).permit(:title, :description))
       flash[:notice] = "Article was updated"
       redirect_to article_path(@article)
@@ -37,8 +38,13 @@ class ArticlesController < ApplicationController
     end
   end
   def destroy
-    @article = Article.find(params[:id])
+    
     @article.destroy
     redirect_to articles_path
+  end
+
+  private
+  def set_article
+    @article = Article.find(params[:id])
   end
 end
