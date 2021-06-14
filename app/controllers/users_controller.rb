aea23ebefbf0e378ef1485ebc5ba5bbdadd316b3
Dhruv Kapoor <dhruvkapoor01@gmail.com>
2021-06-14 18:10:05 +0530

    Add edit template and update action

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -17,6 +17,7 @@ class UsersController < ApplicationController
 
   # GET /users/1/edit
   def edit
+    @user = User.find(params[:id])
   end
 
   # POST /users or /users.json
@@ -33,14 +34,12 @@ class UsersController < ApplicationController
 
   # PATCH/PUT /users/1 or /users/1.json
   def update
-    respond_to do |format|
-      if @user.update(user_params)
-        format.html { redirect_to @user, notice: "User was successfully updated." }
-        format.json { render :show, status: :ok, location: @user }
-      else
-        format.html { render :edit, status: :unprocessable_entity }
-        format.json { render json: @user.errors, status: :unprocessable_entity }
-      end
+    @user = User.new(user_params)
+    if @user.update(user_params)
+      flash[:notice] = "Your Account information was successfully updated." 
+      redirect_to articles_path
+    else
+      render 'edit'
     end
   end
 
