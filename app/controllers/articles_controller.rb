6e3e090307bb43fe719f2d1966da4469a4b0eefb
Dhruv Kapoor <dhruvkapoor01@gmail.com>
2021-06-14 13:38:50 +0530

    Show user info in article commit

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -19,6 +19,7 @@ before_action :set_article, only: [:show, :edit, :update, :destroy]
 
   def create
     @article = Article.new(params.require(:article).permit(:title, :description))
+    @article.user = User.first
     if @article.save
       flash[:notice] = "Article was created successfully"
       redirect_to article_path(@article)
