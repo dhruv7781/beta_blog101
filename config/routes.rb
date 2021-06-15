33c19d68856845eb93294dbef0ebff8cc26670e9
Dhruv Kapoor <dhruvkapoor01@gmail.com>
2021-06-16 00:20:12 +0530

    log in form init commit

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,8 @@ Rails.application.routes.draw do
   get 'about', to: 'pages#about'
   resources :articles, only: [:show, :index, :new, :create, :edit, :update, :destroy]
   get 'signup', to: 'users#new'
-  post 'users', to: 'users#create'
   resources :users, except: [:new]
+  get 'login', to: 'sessions#new'
+  post 'login', to: 'sessions#create'
+  delete 'login', to: 'sessions#destroy'
 end
