c8fa78476815621db63ef813dd1b043dda910548
Dhruv Kapoor <dhruvkapoor01@gmail.com>
2021-06-16 15:19:30 +0530

    Helper authentication

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -26,6 +26,7 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
+      session[:user_id] = @user.id
       flash[:notice] = "Welcome  to beta blog 101 #{@user.username}, you have signed up." 
       redirect_to articles_path
     else
